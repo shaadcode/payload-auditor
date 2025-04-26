ca0f18b0e26d94eb48a9ab5f0684536aeed94242
seyed <shaadcode@gmail.come>
2025-04-26 17:01:16 +0330

    feat(hooks): add all supported operations for the `afterOperation` hook

--- src/core/log-builders/collections/afterOperation/afterOperation.ts
+++ src/core/log-builders/collections/afterOperation/afterOperation.ts
@@ -1,25 +1,132 @@
 import type { CollectionAfterOperationHook } from 'payload'
 
+import type { ActivityLog } from './../../../../collections/activity-logs.js'
+import type { TrackedCollection } from './../../../../types/pluginOptions.js'
+
+import { emitEvent } from './../../../../core/events/emitter.js'
+
 const afterOperationCollectionLogBuilder: CollectionAfterOperationHook = ({
   args,
   collection,
+  // @ts-ignore
   context,
   operation,
   req,
   result,
 }) => {
-  console.log(context)
-  // if ((context.userHookConfig as TrackedCollection).hooks?.afterDelete?.delete?.enabled) {
-  //   const log: ActivityLog = {
-  //     action: 'delete',
-  //     collection: collection.slug,
-  //     documentId: 'unknown',
-  //     timestamp: new Date(),
-  //     user: req?.user?.id || null,
-  //     userAgent: req.headers.get('user-agent') || 'unknown',
-  //   }
-  //   emitEvent('logGenerated', log)
-  // }
+  if ((context.userHookConfig as TrackedCollection).hooks?.afterOperation?.create?.enabled) {
+    const log: ActivityLog = {
+      action: 'create',
+      collection: collection.slug,
+      documentId: 'unknown',
+      timestamp: new Date(),
+      user: req?.user?.id || null,
+      userAgent: req.headers.get('user-agent') || 'unknown',
+    }
+    emitEvent('logGenerated', log)
+  }
+  if ((context.userHookConfig as TrackedCollection).hooks?.afterOperation?.delete?.enabled) {
+    const log: ActivityLog = {
+      action: 'delete',
+      collection: collection.slug,
+      documentId: 'unknown',
+      timestamp: new Date(),
+      user: req?.user?.id || null,
+      userAgent: req.headers.get('user-agent') || 'unknown',
+    }
+    emitEvent('logGenerated', log)
+  }
+  if ((context.userHookConfig as TrackedCollection).hooks?.afterOperation?.deleteByID?.enabled) {
+    const log: ActivityLog = {
+      action: 'deleteByID',
+      collection: collection.slug,
+      documentId: 'unknown',
+      timestamp: new Date(),
+      user: req?.user?.id || null,
+      userAgent: req.headers.get('user-agent') || 'unknown',
+    }
+    emitEvent('logGenerated', log)
+  }
+  if ((context.userHookConfig as TrackedCollection).hooks?.afterOperation?.find?.enabled) {
+    const log: ActivityLog = {
+      action: 'find',
+      collection: collection.slug,
+      documentId: 'unknown',
+      timestamp: new Date(),
+      user: req?.user?.id || null,
+      userAgent: req.headers.get('user-agent') || 'unknown',
+    }
+    emitEvent('logGenerated', log)
+  }
+  if ((context.userHookConfig as TrackedCollection).hooks?.afterOperation?.findByID?.enabled) {
+    const log: ActivityLog = {
+      action: 'findByID',
+      collection: collection.slug,
+      documentId: 'unknown',
+      timestamp: new Date(),
+      user: req?.user?.id || null,
+      userAgent: req.headers.get('user-agent') || 'unknown',
+    }
+    emitEvent('logGenerated', log)
+  }
+  if (
+    (context.userHookConfig as TrackedCollection).hooks?.afterOperation?.forgotPassword?.enabled
+  ) {
+    const log: ActivityLog = {
+      action: 'forgotPassword',
+      collection: collection.slug,
+      documentId: 'unknown',
+      timestamp: new Date(),
+      user: req?.user?.id || null,
+      userAgent: req.headers.get('user-agent') || 'unknown',
+    }
+    emitEvent('logGenerated', log)
+  }
+  if ((context.userHookConfig as TrackedCollection).hooks?.afterOperation?.login?.enabled) {
+    const log: ActivityLog = {
+      action: 'login',
+      collection: collection.slug,
+      documentId: 'unknown',
+      timestamp: new Date(),
+      user: req?.user?.id || null,
+      userAgent: req.headers.get('user-agent') || 'unknown',
+    }
+    emitEvent('logGenerated', log)
+  }
+  if ((context.userHookConfig as TrackedCollection).hooks?.afterOperation?.refresh?.enabled) {
+    const log: ActivityLog = {
+      action: 'refresh',
+      collection: collection.slug,
+      documentId: 'unknown',
+      timestamp: new Date(),
+      user: req?.user?.id || null,
+      userAgent: req.headers.get('user-agent') || 'unknown',
+    }
+    emitEvent('logGenerated', log)
+  }
+  if ((context.userHookConfig as TrackedCollection).hooks?.afterOperation?.update?.enabled) {
+    const log: ActivityLog = {
+      action: 'update',
+      collection: collection.slug,
+      documentId: 'unknown',
+      timestamp: new Date(),
+      user: req?.user?.id || null,
+      userAgent: req.headers.get('user-agent') || 'unknown',
+    }
+    emitEvent('logGenerated', log)
+  }
+  if ((context.userHookConfig as TrackedCollection).hooks?.afterOperation?.updateByID?.enabled) {
+    const log: ActivityLog = {
+      action: 'updateByID',
+      collection: collection.slug,
+      documentId: 'unknown',
+      timestamp: new Date(),
+      user: req?.user?.id || null,
+      userAgent: req.headers.get('user-agent') || 'unknown',
+    }
+    emitEvent('logGenerated', log)
+  }
+
   return result
 }
 
