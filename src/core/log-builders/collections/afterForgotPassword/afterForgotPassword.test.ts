eab4c9f650b4a56275b43d6ca2a05974b9227118
seyed <shaadcode@gmail.come>
2025-05-05 17:59:46 +0330

    test(hooks): added unit test for afterForgotPassword hook

---
+++ src/core/log-builders/collections/afterForgotPassword/afterForgotPassword.test.ts
@@ -0,0 +1,110 @@
+import type { RequestContext } from 'next/dist/server/base-server.js'
+import type { CollectionAfterForgotPasswordHook, SanitizedCollectionConfig } from 'payload'
+import type { AuditorLog } from 'src/collections/auditor.js'
+import type { TrackedCollection } from 'src/types/pluginOptions.js'
+
+import { emitEvent } from 'src/core/events/emitter.js'
+import afterForgotPasswordCollectionLogBuilder from 'src/core/log-builders/collections/afterForgotPassword/afterForgotPassword.js'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+
+vi.mock('src/core/events/emitter.js', () => ({
+  emitEvent: vi.fn(),
+}))
+
+describe('afterError collection hook', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+  it('Should not log if forgot password operation is not enabled', async () => {
+    const mockArgs = {
+      args: {},
+      collection: {
+        slug: 'test',
+      } as SanitizedCollectionConfig,
+      context: {
+        userHookConfig: {
+          hooks: {
+            afterForgotPassword: {
+              forgotPassword: {
+                enabled: false,
+              },
+            },
+          },
+        } as TrackedCollection,
+      } as unknown as RequestContext,
+    } as Parameters<CollectionAfterForgotPasswordHook>[0]
+    const result = await afterForgotPasswordCollectionLogBuilder(mockArgs)
+
+    expect(result).toEqual({})
+
+    expect(emitEvent).toHaveBeenCalledTimes(0)
+  })
+
+  it('Should log if forgot password operation is enabled', async () => {
+    const mockArgs = {
+      args: {
+        data: {
+          email: 'test@example.com',
+        },
+        req: {
+          headers: {
+            get: vi.fn().mockReturnValue('test-agent'),
+          },
+          payload: {
+            find: vi.fn().mockResolvedValue({
+              docs: [{ id: 'userId' }],
+            }),
+          },
+        },
+      },
+      collection: {
+        slug: 'test',
+      } as SanitizedCollectionConfig,
+      context: {
+        userHookConfig: {
+          hooks: {
+            afterForgotPassword: {
+              forgotPassword: {
+                enabled: true,
+              },
+            },
+          },
+        } as TrackedCollection,
+      } as unknown as RequestContext,
+    } as Parameters<CollectionAfterForgotPasswordHook>[0]
+    const result = await afterForgotPasswordCollectionLogBuilder(mockArgs)
+
+    expect(result).toEqual({})
+    expect(emitEvent).toHaveBeenCalledTimes(1)
+
+    const expectedLog: AuditorLog = {
+      action: 'forgotPassword',
+      collection: 'test',
+      timestamp: expect.any(Date),
+      user: 'userId',
+      userAgent: 'test-agent',
+    }
+    expect(emitEvent).toHaveBeenCalledWith('logGenerated', expectedLog)
+  })
+
+  it('Should not log, it is wrong when the hook is activated', async () => {
+    const mockArgs = {
+      args: {},
+      collection: {
+        slug: 'test',
+      } as SanitizedCollectionConfig,
+      context: {
+        userHookConfig: {
+          hooks: {
+            afterLogin: {},
+          },
+        } as TrackedCollection,
+      } as unknown as RequestContext,
+    } as Parameters<CollectionAfterForgotPasswordHook>[0]
+    const result = await afterForgotPasswordCollectionLogBuilder(mockArgs)
+
+    expect(result).toEqual({})
+
+    expect(emitEvent).toHaveBeenCalledTimes(0)
+  })
+})
