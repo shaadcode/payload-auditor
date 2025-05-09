9296040171a4584f5f7cd3882cf9d8a5dc6d589b
seyed <shaadcode@gmail.come>
2025-05-09 11:13:32 +0330

    test(hooks): added unit test for afterMe hook

--- src/core/log-builders/collections/afterMe/afterMe.test.ts
+++ src/core/log-builders/collections/afterMe/afterMe.test.ts
@@ -1,21 +1,21 @@
 import type { RequestContext } from 'next/dist/server/base-server.js'
-import type { CollectionAfterLogoutHook, SanitizedCollectionConfig } from 'payload'
+import type { CollectionAfterMeHook, PayloadRequest, SanitizedCollectionConfig } from 'payload'
 import type { AuditorLog } from 'src/collections/auditor.js'
 import type { TrackedCollection } from 'src/types/pluginOptions.js'
 
 import { emitEvent } from 'src/core/events/emitter.js'
-import afterLogoutCollectionLogBuilder from 'src/core/log-builders/collections/afterLogout/afterLogout.js'
+import afterMeCollectionLogBuilder from 'src/core/log-builders/collections/afterMe/afterMe.js'
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 
 vi.mock('src/core/events/emitter.js', () => ({
   emitEvent: vi.fn(),
 }))
 
-describe('afterLogout collection hook', () => {
+describe('afterMe collection hook', () => {
   beforeEach(() => {
     vi.clearAllMocks()
   })
-  it('Should not log if logout operation is not enabled', async () => {
+  it('Should not log if me operation is not enabled', async () => {
     const mockArgs = {
       collection: {
         slug: 'users',
@@ -23,8 +23,8 @@ describe('afterLogout collection hook', () => {
       context: {
         userHookConfig: {
           hooks: {
-            afterLogout: {
-              logout: {
+            afterMe: {
+              me: {
                 enabled: false,
               },
             },
@@ -35,16 +35,17 @@ describe('afterLogout collection hook', () => {
         headers: {
           get: () => 'test-agent',
         },
-      },
-    } as unknown as Parameters<CollectionAfterLogoutHook>[0]
-    const result = await afterLogoutCollectionLogBuilder(mockArgs)
+      } as unknown as PayloadRequest,
+      response: {} as unknown,
+    } as Parameters<CollectionAfterMeHook>[0]
+    const result = await afterMeCollectionLogBuilder(mockArgs)
 
     expect(result).toEqual({})
 
     expect(emitEvent).toHaveBeenCalledTimes(0)
   })
 
-  it('Should not log if logout operation is not defined', async () => {
+  it('Should not log if me operation is not defined', async () => {
     const mockArgs = {
       collection: {
         slug: 'users',
@@ -52,7 +53,7 @@ describe('afterLogout collection hook', () => {
       context: {
         userHookConfig: {
           hooks: {
-            afterLogout: {},
+            afterMe: {},
           },
         } as TrackedCollection,
       } as unknown as RequestContext,
@@ -60,55 +61,17 @@ describe('afterLogout collection hook', () => {
         headers: {
           get: () => 'test-agent',
         },
-      },
-    } as unknown as Parameters<CollectionAfterLogoutHook>[0]
-    const result = await afterLogoutCollectionLogBuilder(mockArgs)
+      } as unknown as PayloadRequest,
+      response: {} as unknown,
+    } as Parameters<CollectionAfterMeHook>[0]
+    const result = await afterMeCollectionLogBuilder(mockArgs)
 
     expect(result).toEqual({})
 
     expect(emitEvent).toHaveBeenCalledTimes(0)
   })
 
-  it('Should log if logout operation is enabled', async () => {
-    const mockArgs = {
-      collection: {
-        slug: 'users',
-      } as SanitizedCollectionConfig,
-      context: {
-        userHookConfig: {
-          hooks: {
-            afterLogout: {
-              logout: {
-                enabled: true,
-              },
-            },
-          },
-        } as TrackedCollection,
-      } as unknown as RequestContext,
-      req: {
-        headers: {
-          get: () => 'test-agent',
-        },
-        user: {
-          id: 'id-123',
-        },
-      },
-    } as unknown as Parameters<CollectionAfterLogoutHook>[0]
-    const result = await afterLogoutCollectionLogBuilder(mockArgs)
-    expect(result).toEqual({})
-    expect(emitEvent).toHaveBeenCalledTimes(1)
-
-    const expectedLog: AuditorLog = {
-      action: 'logout',
-      collection: 'users',
-      timestamp: expect.any(Date),
-      user: 'id-123',
-      userAgent: 'test-agent',
-    }
-    expect(emitEvent).toHaveBeenCalledWith('logGenerated', expectedLog)
-  })
-
-  it('Should not log, it is wrong when the hook is activated', async () => {
+  it('Should log if me operation is enabled', async () => {
     const mockArgs = {
       collection: {
         slug: 'users',
@@ -132,8 +95,47 @@ describe('afterLogout collection hook', () => {
           id: 'id-123',
         },
       },
-    } as unknown as Parameters<CollectionAfterLogoutHook>[0]
-    const result = await afterLogoutCollectionLogBuilder(mockArgs)
+    } as unknown as Parameters<CollectionAfterMeHook>[0]
+    const result = await afterMeCollectionLogBuilder(mockArgs)
+    expect(result).toEqual({})
+    expect(emitEvent).toHaveBeenCalledTimes(1)
+
+    const expectedLog: AuditorLog = {
+      action: 'me',
+      collection: 'users',
+      timestamp: expect.any(Date),
+      user: 'id-123',
+      userAgent: 'test-agent',
+    }
+    expect(emitEvent).toHaveBeenCalledWith('logGenerated', expectedLog)
+  })
+
+  it('Should not log, it is wrong when the hook is activated', async () => {
+    const mockArgs = {
+      collection: {
+        slug: 'users',
+      } as SanitizedCollectionConfig,
+      context: {
+        userHookConfig: {
+          hooks: {
+            beforeValidate: {
+              create: {
+                enabled: true,
+              },
+            },
+          },
+        } as TrackedCollection,
+      } as unknown as RequestContext,
+      req: {
+        headers: {
+          get: () => 'test-agent',
+        },
+        user: {
+          id: 'id-123',
+        },
+      },
+    } as unknown as Parameters<CollectionAfterMeHook>[0]
+    const result = await afterMeCollectionLogBuilder(mockArgs)
 
     expect(result).toEqual({})
 
